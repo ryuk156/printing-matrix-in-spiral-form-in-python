084bb4e96ecf51bd786386b148722f56f9b5bddc
Yash Patel <49101492+yash15601@users.noreply.github.com>
2020-04-18 22:58:25 +0530

    Update matrixinspiral.py

--- matrixinspiral.py
+++ matrixinspiral.py
@@ -6,22 +6,23 @@ x=[[1,2,3,11],
 
 
 
-k=0
-l=0
-m=3
-n=4
+k=0 ##starting row 
+l=0 ##staring col
+m=3 ##number of rows
+n=4 ##number of col
 
 
-while(k<m and l<n):
-    for i in range(l,n):
+while(k<m and l<n):     
+    for i in range(l,n):      
         print(x[k][i])
 
     k=k+1
 
-    for i in range(k,m):
+    for i in range(k,m):     
         print(x[i][n-1])
 
     n=n-1
+   
     if(k<m):
         for i in range(n-1,l-1,-1):
             print(x[m-1][i])
@@ -38,27 +39,6 @@ while(k<m and l<n):
 
 
 
-####first row11
-##for i in range(l,n):
-##    print(x[k][i])
-##
-##
-####now go in second row by increamenting m by 1     
-##k=1
-##for i in range(l,m):
-##    print(x[i][n-1])
-##
-##
-##n=n-1
-##
-##for i in range(n-1,l-1,-1):
-##    
-##    print(x[m-1][i])
-##m=m-1
-##for i in range(m-1,k-1,-1):
-##    print(x[i][l])
-##
-##l+=1
 
 
 
